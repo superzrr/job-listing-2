f666f41659af7f92e5b5f719c102deedfa15f942
superzrr <superzrr@gmail.com>
2017-04-22 07:26:14 +0800

    implement step6

--- app/models/job.rb
+++ app/models/job.rb
@@ -4,6 +4,9 @@ class Job < ApplicationRecord
   validates :wage_upper_bound,  presence: true
   validates :wage_lower_bound, numericality: { greate_than: 0}
 
+  scope :published, -> { where(is_hidden: false)}
+  scope :recent, -> { order('created_at DESC')}
+
   def publish!
     self.is_hidden = false
     self.save
@@ -13,5 +16,6 @@ class Job < ApplicationRecord
     self.is_hidden = true
     self.save
   end
+
 #  validates :wage_upper_bound, numericality: { greate_then: wage_lower_bound }
 end
