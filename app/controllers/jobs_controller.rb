025769617172eed33731ee754c34bd2550cb273b
superzrr <superzrr@gmail.com>
2017-04-21 21:49:45 +0800

    implement step4

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -2,7 +2,7 @@ class JobsController < ApplicationController
   before_action :authenticate_user!, only: [:edit, :update, :new, :create, :destroy]
 
   def index
-    @jobs = Job.all
+    @jobs = Job.where(:is_hidden => false).order("created_at DESC")
   end
 
   def show
@@ -46,7 +46,7 @@ class JobsController < ApplicationController
 
   private
   def job_params
-    params.require(:job).permit(:title, :description,:wage_lower_bound, :wage_upper_bound, :contact_email)
+    params.require(:job).permit(:title, :description,:wage_lower_bound, :wage_upper_bound, :contact_email, :is_hidden)
   end
 
 end
--- app/models/job.rb
+++ app/models/job.rb
@@ -3,5 +3,6 @@ class Job < ApplicationRecord
   validates :wage_lower_bound,  presence: true
   validates :wage_upper_bound,  presence: true
   validates :wage_lower_bound, numericality: { greate_than: 0}
+
 #  validates :wage_upper_bound, numericality: { greate_then: wage_lower_bound }
 end
